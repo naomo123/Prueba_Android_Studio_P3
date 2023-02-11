1832227e1020261db83cb794f54077c772525a30
naomo123 <naomoiglesias@gmail.com>
2023-02-11 13:00:07 -0600

    Add radiobuttons

--- app/src/main/java/com/example/p3/MainActivity.kt
+++ app/src/main/java/com/example/p3/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.example.p3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val et1=findViewById<EditText>(R.id.TxtNumero1)
-        val et2=findViewById<EditText>(R.id.TxtNumero2)
-        val tv1=findViewById<TextView>(R.id.TxtVResult)
-        val button=findViewById<Button>(R.id.BtnOperacion)
+        val et1=findViewById<EditText>(R.id.txtNumero1)
+        val et2=findViewById<EditText>(R.id.txtNumero2)
+        var tv1=findViewById<TextView>(R.id.TxtVResult)
+        val RGBtn=findViewById<RadioGroup>(R.id.radioGroup)
+        val rbtn1=findViewById<RadioButton>(R.id.rbtnSumar)
+        val rbtn2=findViewById<RadioButton>(R.id.rbtnRestar)
+        val rbtn3=findViewById<RadioButton>(R.id.rbtnMulti)
+        val rbtn4=findViewById<RadioButton>(R.id.rbtnDiv)
+
+        et1.setInputType(InputType.TYPE_CLASS_NUMBER);
+        et2.setInputType(InputType.TYPE_CLASS_NUMBER);
+        val button=findViewById<Button  >(R.id.BtnOperacion)
+
+        button.setOnClickListener(View.OnClickListener {
+            if(rbtn1.isChecked())
+            tv1.setText("El resultado es "+ (et1.text.toString().toInt() + et2.text.toString().toInt()))
+            if(rbtn2.isChecked())
+                tv1.setText("El resultado es "+ (et1.text.toString().toInt() - et2.text.toString().toInt()))
+            if(rbtn3.isChecked())
+                tv1.setText("El resultado es "+ (et1.text.toString().toInt() * et2.text.toString().toInt()))
+            if(rbtn4.isChecked())
+                tv1.setText("El resultado es "+ (et1.text.toString().toInt() / et2.text.toString().toInt()))
+
+        })
+    }
 
 
 
@@ -22,4 +47,3 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-}
